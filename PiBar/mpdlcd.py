d7fb183715ddc68ae0e56deec53359322d5ca2ab
Rick Schnorenberg <rjschnorenberg@gmail.com>
2024-02-12 20:19:51 -0600

    Update to python mpd library and latest adafruit lcd library

--- PiBar/mpdlcd.py
+++ PiBar/mpdlcd.py
@@ -1,69 +1,69 @@
 #!/usr/bin/python
-import math
-import time
-import re
-
 from time import sleep
 from os import popen
+from mpd import MPDClient
 
-import Adafruit_CharLCD as LCD
+import board
+import digitalio
+import pwmio
+import adafruit_character_lcd.character_lcd as characterlcd
 
 # Raspberry Pi configuration:
-lcd_rs = 27  # Change this to pin 21 on older revision Raspberry Pi's
-lcd_en = 22
-lcd_d4 = 25
-lcd_d5 = 24
-lcd_d6 = 23
-lcd_d7 = 18
-lcd_red   = 4
-lcd_green = 17
-lcd_blue  = 7  # Pin 7 is CE1
+lcd_rs = digitalio.DigitalInOut(board.D27)
+lcd_en = digitalio.DigitalInOut(board.D22)
+lcd_d7 = digitalio.DigitalInOut(board.D18)
+lcd_d6 = digitalio.DigitalInOut(board.D23)
+lcd_d5 = digitalio.DigitalInOut(board.D24)
+lcd_d4 = digitalio.DigitalInOut(board.D25)
+red = pwmio.PWMOut(board.D4)
+green = pwmio.PWMOut(board.D17)
+blue = pwmio.PWMOut(board.D7)
+lcd_columns = 16
+lcd_rows = 2
 
 # Define LCD column and row size for 16x2 LCD.
 lcd_columns = 16
 lcd_rows    = 2
 
 # Initialize the LCD using the pins above.
-lcd = LCD.Adafruit_RGBCharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_red, lcd_green, lcd_blue)
+lcd = characterlcd.Character_LCD_RGB(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, red, green, blue)
 
-re_volume = re.compile('volume:\s*(\d+)%')
-
-previous_title = ''
+previous_title = ""
 previous_volume = 0
-previous_state = ''
+previous_state = ""
+
+mpdClient = MPDClient()
+mpdClient.connect("localhost", 6600)
 
 while True:
-  mpc_proc = popen('mpc -f "%title%"') 
-  title = mpc_proc.readline().rstrip()
-  state = mpc_proc.readline().rstrip().split(' ')[0]
+  status = mpdClient.status()
+  song = mpdClient.playlistid(status["songid"])[0]
+  title = song["title"]
+  state = status["state"]
   if state != previous_state:
     previous_state = state
-    if state != '[playing]':
-      lcd.set_color(0, 0, 0)
+    if state != "play":
+      lcd.color = [0, 0, 0]
       lcd.clear()
-      popen('i2cset -y 1 0x4b 0 0')
+      popen("i2cset -y 1 0x4b 0 0")
     else:
-      lcd.set_color(1.0, 1.0, 1.0)
-      previous_title = ''
+      lcd.color = [100, 100, 100]
+      previous_title = ""
       previous_volume = 0
 
-  if state == '[playing]':
-    mpc_proc = popen('mpc volume')
-    volume = int(re_volume.match(mpc_proc.readline()).group(1))
+  if state == "play":
+    volume = int(status["volume"])
     if volume != previous_volume:
       previous_volume = volume
       if volume == 0:
-        popen('i2cset -y 1 0x4b 0 0')
+        popen("i2cset -y 1 0x4b 0 0")
       else:
-        popen('i2cset -y 1 0x4b 0 ' + str((volume / 10) + 25))
-   
+        popen("i2cset -y 1 0x4b 0 " + str(round(volume / 4) + 10))
+
     if title != previous_title:
       previous_title = title
-      mpc_proc = popen('mpc -f "%artist%"')
-      artist = mpc_proc.readline().rstrip()
+      artist = song["artist"]
       lcd.clear()
-      lcd.message(artist)
-      lcd.message('\n')
-      lcd.message(title)
-   
+      lcd.message = artist + "\n" + title
+
   sleep(1)
